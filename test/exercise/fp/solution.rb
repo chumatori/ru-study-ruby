5c055bbc1f228c8c1deb241b77155f8434c69f07
Semyon Turitsyn <s.turitsyn@quickinim.com>
2024-02-01 23:10:41 +0400

    fix PR comments

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -5,32 +5,26 @@ module Exercise
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
       def rating(array)
-        valid_films = array.map { |film| film if sum_rating_validate(film) }.compact
+        valid_films = array.map { |film| film if film_params_valid?(film) }.compact
         valid_films.reduce(0) { |acc, film| acc + film['rating_kinopoisk'].to_f } / valid_films.count
       end
 
       def chars_count(films, threshold, char = 'и')
         films.reduce(0) do |acc, film|
-          if film['rating_kinopoisk'].to_f >= threshold
-            acc + film['name'].count(char)
-          else
-            acc
-          end
+          film['rating_kinopoisk'].to_f >= threshold ? acc + film['name'].count(char) : acc
         end
       end
 
       private
 
-      def method_name; end
-
-      def sum_rating_validate(film)
+      def film_params_valid?(film)
         multiple_countries?(film) && rating_valid?(film)
       end
 
       def multiple_countries?(film)
         return false if film['country'].nil?
 
-        film['country'].include?(',')
+        film['country'].split(',').count > 1
       end
 
       def rating_valid?(film)
